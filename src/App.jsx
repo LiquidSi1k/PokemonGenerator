162e081222889cefaf72efc29f3b862011ca7323
Voo Tzuen Hau <vince_voo@yahoo.com>
2023-08-21 16:40:45 +0800

    pokemon generator

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [randomName, setRandomName] = useState("");
+  const [randomImageUrl, setRandomImageUrl] = useState("");
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemonList(data.results);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const generateRandomName = () => {
+    const randomIndex = Math.floor(Math.random() * pokemonList.length);
+    const randomPokemon = pokemonList[randomIndex];
+    setRandomName(randomPokemon.name);
+    setRandomImageUrl(
+      `https://img.pokemondb.net/artwork/large/${randomPokemon.name}.jpg`
+    );
+  };
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Pokemon Friends</h1>
+      <button className="btn" onClick={generateRandomName}>
+        Click for a Pokemon Friend
+      </button>
+      <h1>Pokemon Name: {randomName}</h1>
+      <br />
+      {randomImageUrl && (
+        <img
+          src={randomImageUrl}
+          height="300"
+          width="350"
+          alt="Random Pokemon"
+          style={{ objectFit: "scale-down" }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
